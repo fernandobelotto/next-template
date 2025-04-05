60591a7e0745db79c64394373757cac82bee6835
Fernando Belotto <fernando.bbosco@gmail.com>
2025-04-05 00:07:23 -0300

    react query and repomix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ReactQueryProvider } from "@/components/react-query-provider";
 import { AppSidebar } from "@/components/sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,13 +19,15 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<SidebarProvider>
-					<AppSidebar />
-					<main className="flex-1 p-2">
-						<SidebarTrigger />
-						{children}
-					</main>
-				</SidebarProvider>
+				<ReactQueryProvider>
+					<SidebarProvider>
+						<AppSidebar />
+						<main className="flex-1 p-2">
+							<SidebarTrigger />
+							{children}
+						</main>
+					</SidebarProvider>
+				</ReactQueryProvider>
 			</body>
 		</html>
 	);
---
+++ src/components/react-query-provider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export function ReactQueryProvider({
+	children,
+}: { children: React.ReactNode }) {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
+			{children}
+		</QueryClientProvider>
+	);
+}
